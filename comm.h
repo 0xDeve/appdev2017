f43d412aee1edb9aa587dee26f439d8d6ad9adea
andretom1 <thanhlong130298@gmail.com>
2017-04-20 16:00:23 +0300

    new file:   comm.c 	new file:   comm.h 	new file:   comm.o 	new file:   main.c 	new file:   main.o 	new file:   screen.c 	new file:   screen.h 	new file:   screen.o 	new file:   wave.c 	new file:   wave.h 	new file:   wave.o

---
+++ comm.h
@@ -0,0 +1,7 @@
+// In this header we define some necessary constants for CURL communication
+
+#define COMM
+
+#define URL "http://www.cc.puv.fi/~e1601113/appdev2017/raspsum.php"
+
+void send_data_curl(double *);
